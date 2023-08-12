865525fb8c6be91e51a8574088647b6095d511cf
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:18:24 +0700

    Update Animals.java
    delete comment

--- main/java/turorial/javalombok/Animals.java
+++ main/java/turorial/javalombok/Animals.java
@@ -5,11 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
-@RequiredArgsConstructor//membuat constructor dengan field yang kita mau tapi field nya harus di buat final
-//@RequiredArgsConstructor(staticName = "isRun") ini kalau kita ingin membuat method static
+@RequiredArgsConstructor
 public class Animals {
-    //tapi perlu di ingat karena kedua field ini final maka dia tidak akan ada setter nya kalau getter nya ada
-    private final String name; //name akan menjadi parameter di constructor nya karna final
-    private final String sound; //sound akan menjadi parameter di constructor nya karna final
+    private final String name;
+    private final String sound;
     private String run;
 }
