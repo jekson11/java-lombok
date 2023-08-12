96c1a04454bccd062fb147826b2a66a3bc6577b1
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:19:01 +0700

    Update Counter.java
    delete comment

--- main/java/turorial/javalombok/Counter.java
+++ main/java/turorial/javalombok/Counter.java
@@ -4,11 +4,7 @@ import lombok.Synchronized;
 
 public class Counter {
     private static Integer value = 0;
-
-//    @Synchronized //jkalau begini jadinya semua yang ada di method ini di synchronize jadi tidak flexsible
-    //pada java thread kita sudah bahas bagaimana membuat kode synchronize yang lebih baik
-    @Synchronized(value = "value") //kita bisa sharing lock seperti ini jadi buat valuenya field mana ynag mau di synchronize
-    //kita bisa buat ini di semua method yang memuliki field value misalnya
+    @Synchronized(value = "value") 
     public static void increment(){
         value++;
     }
