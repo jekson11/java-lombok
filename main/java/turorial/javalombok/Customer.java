ab9d26018150e16889250ed67a2178ab2b9f5e68
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:19:34 +0700

    Update Customer.java
    delete comment

--- main/java/turorial/javalombok/Customer.java
+++ main/java/turorial/javalombok/Customer.java
@@ -1,12 +1,7 @@
 package turorial.javalombok;
 
 import lombok.*;
-
-//kalau kita membuat nya di class nya maka secara otomatis semua field nya akan punya getter dan setter
-@Getter()//kalau begini ini default nya public
-//@EqualsAndHashCode //ini akan membuta semua field nya di equals dan di hashCode
-//dengan begini name nya tidak akan di equals dan di hashCode ini jika kita ingin beberapa
-//field tidak di equals dan di hashCode ini array y tinggal tambahkan {} di dalam itu kita bebas mau masukkan berap field
+@Getter()
 @EqualsAndHashCode(exclude = "name")
 @AllArgsConstructor
 @NoArgsConstructor
