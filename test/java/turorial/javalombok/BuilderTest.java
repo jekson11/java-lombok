f55179b2104b381057ea289f73970bfa6bf211ce
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:12:56 +0700

    Update BuilderTest.java
    delete comment

--- test/java/turorial/javalombok/BuilderTest.java
+++ test/java/turorial/javalombok/BuilderTest.java
@@ -27,22 +27,15 @@ public class BuilderTest {
         vehicle1.setBrand("Honda");
         vehicle1.setColors(colors);
         Assertions.assertNotNull(vehicle1.getName());
-
-        //inikan ribet terlalubanya nah dengan menggunakan @Builder annotation pada class nya Vehicle yang kita buat
-        //kita bisa membuat nya lebih simple dan mudah di baca
     }
 
     @Test
     void testVehicleWithBuilder(){
-        //ini jika kita menambahkan annotation @Builder
         List<String> colors = new ArrayList<>();
         colors.add("Black");
         colors.add("Blue");
         colors.add("White");
-
-        //dia akan otomatis membuat method builder dan build di mana build untuk menutup dan builder untuk membuka
-        //di dalamnya kita bisa langsung chining data object nya tanpa menggunakan objec class nya
-        var vehicle = Vehicle.builder()//menggunakan method builder dan di akhiri dengan method build
+        var vehicle = Vehicle.builder()
                 .name("Car").brand("Honda").colors(colors).price(200000000L)
                 .build();
 
@@ -51,12 +44,6 @@ public class BuilderTest {
 
     @Test
     void testVehicleWithBuilderAndSingular(){
-        //dan ini jika kita menambahkan annotation @Singular pada field yang bertipe data container seperti List Set dll
-        //dengan kita tambahkan annotation itu kita tidak perlu lagi membuat List nya untuk mengisi field color yang
-        //bertipe data List kita bisa langsung meng chining nya di dalam method builder nya method color itu otomatis
-        //di buat sesuai nama field List yang kita buat misal namanya colors maka akan di buat method color misal lagi
-        //namanya hobbies maka akan di buat method hobby jadi nama field nya usahakan harus merepresantasikan
-        //lebih sari satu y
         Vehicle vehicle = Vehicle.builder()
                 .name("Car").price(200000000L).brand("Honda")
                 .color("blue").color("black").color("white")
