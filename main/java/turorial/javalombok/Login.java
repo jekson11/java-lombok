1f444e178199249ce0cee8449ac17796b1c3531c
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:20:42 +0700

    Update Login.java
    delete comment

--- main/java/turorial/javalombok/Login.java
+++ main/java/turorial/javalombok/Login.java
@@ -4,11 +4,8 @@ import lombok.*;
 
 @Getter
 @Setter
-@NoArgsConstructor//membuat costructor tanpa parameter
-@AllArgsConstructor//membut costructor dengan semua parameter yang ada di class nya
-//@ToString //kalau begini semua field yang ada pada class akan di keluarkan di method toString
-//ini akan membuat method toString tapi untuk field password tidak akan di toString ini bergunak
-//ketika ada field yang bersifat rahasia dan kita tidak ingin menampilkannya jadi yang tampil ini hanya username saja
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString(exclude = "password")
 public class Login {
     private String username;
