ff81604d5266158bbbd7b357e4974aa91bd37b5e
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:22:12 +0700

    Update Register.java
    delete comment

--- main/java/turorial/javalombok/Register.java
+++ main/java/turorial/javalombok/Register.java
@@ -3,11 +3,7 @@ package turorial.javalombok;
 import lombok.Value;
 import lombok.With;
 
-//dengan menambahkan annotaion @Value itu artinya kita membuat object dan semuanya menjadi immutable yang artinya semua
-//field parameter di constructor nya menjadi final dan untuk method nya hanya getter untuk setter nya tidak ada karena data class ini tidak bisa di ubah/immutable
 @Value
-//denga kita menambahkan ini dia akan membuat method dengan nama withXxx(nama fieldnya) ini ketika kita ingin
-//membuat object yang berbeda tapi yang berbeda itu hanya 1 field nya saja
 @With
 public class Register {
     String username;
