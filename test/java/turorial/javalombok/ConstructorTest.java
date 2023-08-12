c2a3eab96c30f1a50dabe1d9928471f6c9bb43b5
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:14:12 +0700

    Update ConstructorTest.java
    delete comment

--- test/java/turorial/javalombok/ConstructorTest.java
+++ test/java/turorial/javalombok/ConstructorTest.java
@@ -14,16 +14,9 @@ public class ConstructorTest {
 
     @Test
     void testPersonStaticMethod(){
-        //otomatis ini tidak bisa karema harapan kita untuk membuat object class person
-        //harus menggunakan static method yang ada di class person ini costructornya akan menjadi private
-//        Person person = new Person();
-
-        //lihat class Person y
-        Person person = Person.sayHello();//ini yang tanpa parameter
-        assertNull(person.getName());
+        Person person = Person.sayHello();
         assertNull(person.getMessage());
 
-        //pembuatan object class person menggunakan static method class nya
         Person person1 = Person.sayHai("jekson", "Hai..");
         assertNotNull(person1);
         assertEquals("jekson", person1.getName());
@@ -32,9 +25,6 @@ public class ConstructorTest {
 
     @Test
     void testAnimalsRequiredConstructor(){
-        //maka constructornya hanya akan di buat memiliki parameter yang kita mau
-        //tapi ingat kita harus membuat field nya sebagai final untuk menandakan bahwa field tersebut
-        //yang boleh di jadikan parameter pada constructor
         Animals animals = new Animals("Cat", "Miaww");
 
         assertEquals("Cat", animals.getName());
